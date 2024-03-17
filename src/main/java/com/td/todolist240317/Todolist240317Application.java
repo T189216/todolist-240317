a15ff6cf37f2a622dba507b91452a59d1e2aa7b4
seoyeon <tus0926@naver.com>
2024-03-17 21:32:06 +0900

    start

---
+++ src/main/java/com/td/todolist240317/Todolist240317Application.java
@@ -0,0 +1,13 @@
+package com.td.todolist240317;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Todolist240317Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Todolist240317Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/td/todolist240317/Todolist240317ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.td.todolist240317;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Todolist240317ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
